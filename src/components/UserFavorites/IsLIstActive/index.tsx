e123c5582190cfa384e9bf8a246cd9944ee18ea2
11jl98 <joaoferreira981011@gmail.com>
2022-11-08 22:03:47 -0300

    commit

---
+++ src/components/UserFavorites/IsLIstActive/index.tsx
@@ -0,0 +1,97 @@
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default function isActive({ item, isActive }: any) {
+  const navigation = useNavigation();
+
+  return (
+    <>
+      {isActive.includes("playlists") && (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate(
+              "Playlist" as never,
+              {
+                idPlaylist: item.id,
+              } as never
+            )
+          }
+          style={styles.data_playlists}
+        >
+          <View>
+            <Image
+              source={{
+                uri: item?.images[0]?.url
+                  ? item?.images[0]?.url
+                  : "https://community.spotify.com/t5/image/serverpage/image-id/25294i2836BD1C1A31BDF2?v=v2",
+              }}
+              style={styles.images_album}
+            />
+          </View>
+          <View style={styles.container_description}>
+            <Text style={styles.sub_title}>{item.name}</Text>
+            <Text style={styles.desciption}>{item.description}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+      {isActive.includes("artisits") && (
+        <TouchableOpacity
+          // onPress={() =>
+          //   navigation.navigate(
+          //     "Playlist" as never,
+          //     {
+          //       idPlaylist: item.id,
+          //     } as never
+          //   )
+          // }
+          style={styles.data_playlists}
+        >
+          <View>
+            <Image
+              source={{
+                uri: item?.images[0]?.url
+                  ? item?.images[0]?.url
+                  : "https://community.spotify.com/t5/image/serverpage/image-id/25294i2836BD1C1A31BDF2?v=v2",
+              }}
+              style={styles.images_album}
+            />
+          </View>
+          <View style={styles.container_description}>
+            <Text style={styles.sub_title}>{item.name}</Text>
+            { item.genres.map((index:number,e: any)=>
+              <Text style={styles.desciption} key={index}>{e}</Text>
+            )
+
+            }
+          </View>
+        </TouchableOpacity>
+      )}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  images_album: {
+    width: 130,
+    height: 130,
+    margin: 5,
+  },
+  data_playlists: {
+    flexDirection: "row",
+  },
+  container_description: {
+    margin: 10,
+  },
+  sub_title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#fff",
+    marginTop: 15,
+  },
+  desciption: {
+    fontSize: 12,
+    color: "#dcdc",
+    marginTop: 15,
+    maxWidth: 230,
+  },
+});
